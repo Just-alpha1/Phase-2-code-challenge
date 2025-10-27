1f2231338b04732ba2fa729cca1629c3309dd99b
Collins Kiaritha <thuocollins020@gmail.com>
2025-10-27 11:41:34 +0300

    added files in botcollection

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -4,7 +4,8 @@ import BotCard from "./BotCard";
 function BotCollection({ bots, onAddToArmy }) {
   return (
     <div className="bot-collection">
-      <h2>Available Bots</h2>
+      <h2>Available Bots.
+      </h2>
       <div className="bot-list">
         {bots.map((bot) => (
           <BotCard key={bot.id} bot={bot} onAddToArmy={onAddToArmy} />
